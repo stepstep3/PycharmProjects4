9d5923132f494a7c90ab6b54e06b14761ff09555
stepstep3 <71015793+stepstep3@users.noreply.github.com>
2020-12-02 12:42:47 +0400

    123567

---
+++ PycharmProjects/curs1.3 IMPLICIT  WAIT.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+exec_path=r"C:\Users\NASA7\Downloads\chromedriver_win32 (4)\chromedriver.exe"
+URL=r"https://www.wikipedia.org"
+#english_link_locator="js-link-box-en"
+language_locators=["js-link-box-en","js-link-box-ru","js-link-box-de"]
+search_locator="searchInput"
+search_text="Software"
+
+driver=webdriver.Chrome(executable_path=exec_path)
+driver.get(URL)
+driver.implicitly_wait(5)
+driver.maximize_window()
+#1234567
+for i in range(len(language_locators)):
+    language_link=driver.find_element(By.ID, language_locators[i])
+    language_link.click()
+    input_box_element=driver.find_element(By.ID, search_locator)
+    input_box_element.send_keys(search_text)
+    input_box_element.submit()
+    driver.back()
+    driver.back()
+
+time.sleep(10)
+driver.quit()

9ef7cce355b740c3e31939e4b7b35d653ebe157c
stepstep3 <71015793+stepstep3@users.noreply.github.com>
2020-12-03 01:33:22 +0400

    chromedriver

--- untitled5/Test_Cases/cursWikipediaTestClass1.py
+++ untitled5/Test_Cases/cursWikipediaTestClass1.py
@@ -1,7 +1,7 @@
 import unittest
 from selenium import webdriver
-from MainPage import MainPage
-from EnglishPage import EnglishPage
+from untitled5.Pages.MainPage import MainPage
+from untitled5.Pages.EnglishPage import EnglishPage
 
 
 class WikipediaTestClass1(unittest.TestCase):

db06a4820b6ac3aaa7e699959bb5a5f08d103390
stepstep3 <71015793+stepstep3@users.noreply.github.com>
2020-12-02 17:37:58 +0400

    curs 1.2 _

--- untitled5/curs1.2_CLICK_LINK_SEARCH_PHRASE.py
+++ untitled5/curs1.2_CLICK_LINK_SEARCH_PHRASE.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-exec_path=r"C:\Users\NASA7\Downloads\chromedriver_win32 (4)\chromedriver.exe"
+#exec_path=r"C:\Users\NASA7\Downloads\chromedriver_win32 (4)\chromedriver.exe"
 URL=r"https://www.wikipedia.org"
 #english_link_locator="js-link-box-en"
 language_locators=["js-link-box-en","js-link-box-ru","js-link-box-de"]
 search_locator="searchInput"
 search_text="Software"
 
-driver=webdriver.Chrome(executable_path=exec_path)
+driver=webdriver.Chrome()#executable_path=exec_path)
 driver.get(URL)
 driver.maximize_window()
 for i in range(len(language_locators)):
